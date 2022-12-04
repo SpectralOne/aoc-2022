070d9d49dac75656a8668cde1996f959455bdfe5
Artyom Bogachenco <artyombogachenco@gmail.com>
2022-12-04 16:43:34 +0300

    day_3

---
+++ day_3/p1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(const int argc, char** argv) {
+  ifstream cin(argv[1]);
+  string bag;
+  int s = 0;
+
+  while (cin >> bag) {
+    auto half = bag.size() / 2;
+    char uc = *find_first_of(begin(bag), next(begin(bag), half),
+                             next(begin(bag), half), end(bag));
+    s += uc >= 'a' ? uc - 'a' + 1 : uc - 'A' + 27;
+  }
+
+  std::cout << s << endl;
+
+  return 0;
+}
---
+++ day_3/p2.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(const int argc, char** argv) {
+  ifstream cin(argv[1]);
+  string b1, b2, b3;
+  int s = 0;
+
+  while (cin >> b1 && cin >> b2 && cin >> b3) {
+    set<char> a, b, c, abi, r;
+    for (const char ch : b1) a.insert(ch);
+    for (const char ch : b2) b.insert(ch);
+    for (const char ch : b3) c.insert(ch);
+
+    set_intersection(a.begin(), a.end(), b.begin(), b.end(),
+                     inserter(abi, abi.begin()));
+
+    set_intersection(abi.begin(), abi.end(), c.begin(), c.end(),
+                     inserter(r, r.begin()));
+
+    char ch = *r.begin();
+    s += ch >= 'a' ? ch - 'a' + 1 : ch - 'A' + 27;
+  }
+
+  std::cout << s << endl;
+
+  return 0;
+}
