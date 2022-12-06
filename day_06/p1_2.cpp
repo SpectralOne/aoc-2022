d285c8d9b53eaffae3c6d85c8401a61f7d5d803e
Artyom Bogachenco <artyombogachenco@gmail.com>
2022-12-06 23:16:59 +0300

    rewrite as bits

--- day_06/p1_2.cpp
+++ day_06/p1_2.cpp
@@ -9,12 +9,15 @@ int main(const int argc, char** argv) {
   while (cin >> l) {
     int s = 0;
     for (int i = 0; i < l.size() - dist_chars; ++i) {
-      set<char> cs;
+      __int32_t acc = 0UL;
+      for (int j = 0; j < dist_chars; ++j) {
+        __int32_t cur = acc;
 
-      for (int j = 0; j < dist_chars; ++j) cs.insert(l[i + j]);
+        acc |= 1 << l[i + j] - 'a';
 
-      if (cs.size() == dist_chars) break;
-
+        if (cur == acc) break;
+      }
+      if (__builtin_popcount(acc) == dist_chars) break;
       ++s;
     }
     cout << s + dist_chars << endl;
